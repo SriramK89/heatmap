3bc5d468dfb7f1a190be4fb242115133e280e8a5
Sriram K <sriram.k2@tcs.com>
2016-11-07 00:52:16 +0530

    Basic: Working with more options - Updates still needed

---
+++ README.md
@@ -0,0 +1,7 @@
+# Heatmap - A JQuery Plugin
+
+Easy to use JQuery Plugin for implementing Heatmap in a HTML page.
+
+Features:
+* Lightweight
+* Responsive
--- js/heat_map.js
+++ js/heat_map.js
@@ -1,29 +1,50 @@
 $.drawHeatMap = function(domToDraw, dataset, options = {}) {
   var _x = dataset.length;
   var _y = 0;
-  var _canvasDom = $('canvas')
-  _canvasDom.attr('id', '')
+  var _minVal = dataset[0][0];
+  var _maxVal = dataset[0][0];
   $.each(dataset, function(i, value) {
     if(_y < value.length) {
       _y = value.length;
     }
+    $.each(value, function(j, cellData) {
+      if(_minVal > cellData) {
+        _minVal = cellData;
+      }
+      if(_maxVal < cellData) {
+        _maxVal = cellData;
+      }
+    });
   });
-  var _tables = '<table cellspacing="0" style="border:1px solid black;"><tbody>';
+  if(options.range != undefined && options.range.min != undefined && options.range.max != undefined) {
+    if(_minVal > options.range.min) {
+      _minVal = options.range.min;
+    }
+    if(_maxVal < options.range.max) {
+      _maxVal = options.range.max;
+    }
+  }
+  console.log('min:' + _minVal + ', max:' + _maxVal);
   var _i, _j;
   var _domWidth = domToDraw.width();
   var _domHeight = domToDraw.height();
   console.log(dataset);
   console.log('Width: ' + _domWidth);
   console.log('Height: ' + _domHeight);
-  function findColor(length, maxLength)
+  var _tables = '<table cellspacing="0" style="border:1px solid black;';
+  _tables += 'width:' + _domWidth + 'px;height:' + _domHeight + 'px;"><tbody>';
+  function findColor(length)
   {
-    var i = (length * 255 / maxLength);
+    var i = (length * 255 / _maxVal);
     var r = Math.round(Math.sin(0.024 * i + 0) * 127 + 128);
     var g = Math.round(Math.sin(0.024 * i + 2) * 127 + 128);
     var b = Math.round(Math.sin(0.024 * i + 4) * 127 + 128);
     return 'rgb(' + r + ',' + g + ',' + b + ')';
   }
   for(_i = _x;_i >= 0;_i--) {
+    if(options.title != undefined && options.title.show == true && _i == _x && options.title.position == true) {
+      _tables += '<tr><td style="height:20px;text-align:center;" colspan="' + (_y + 2) + '">' + options.title.text + '</td></tr>';
+    }
     _tables += '<tr>';
     for(_j = 0;_j <= _y;_j++) {
       if(_i == 0 && _j == 0) {
@@ -32,31 +53,42 @@ $.drawHeatMap = function(domToDraw, dataset, options = {}) {
         var title = '';
         _tables += '<td style="width:20px;height:20px;';
         if(dataset[_i - 1][_j - 1] > 0) {
-          var color = findColor(dataset[_i - 1][_j - 1], 9);
+          var color = findColor(dataset[_i - 1][_j - 1]);
           _tables += 'background-color:' + color + ';';
           if(options.showToolTip == true) {
-            title = 'title="' + dataset[_i - 1][_j - 1] + '"'
+            title = 'title="';
+            if(options.showLabelOnToolTip == true) {
+              title += options.labels.x[_j - 1] + ',' + options.labels.y[_i - 1] + ':';
+            }
+            title += dataset[_i - 1][_j - 1] + '"';
           }
         }
         _tables += '" ' + title + '>' + '</td>';
       } else if(_j == 0 && _i > 0) {
-        _tables += '<td style="width:20px;height:20px;">';
+        _tables += '<td style="width:20px;height:20px;text-align:center;">';
         if(options.showLabels == true) {
-          _tables += options.labels.y[_x - _i];
+          _tables += options.labels.y[_i - 1];
         }
         _tables += '</td>';
-      } else if(_i == 0){
-        _tables += '<td style="width:20px;height:20px;">';
+      } else if(_i == 0) {
+        _tables += '<td style="width:20px;height:20px;text-align:center;">';
         if(options.showLabels == true) {
           _tables += options.labels.x[_j - 1];
         }
         _tables += '</td>';
       }
+      if(options.showScale == true) {
+        if(_i == _x && _j == _y) {
+          _tables += '<td style="width:20px;" rowspan="' + _x + '"></td>';
+        }
+      }
     }
     _tables += '</tr>';
+    if(options.title != undefined && options.title.show == true && _i == 0 && options.title.position == false) {
+      _tables += '<td style="height:20px;text-align:center;" colspan="' + (_y + 2) + '">' + options.title.text + '</td>';
+    }
   }
   _tables += '</tbody></table>';
   var _doms = $(_tables);
   domToDraw.html(_doms);
-  $(_tables).insertAfter(_canvasDom);
 }
